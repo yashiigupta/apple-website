ba3703a95f5ff4ee87bb2294f189d9f6b606a0fa
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-12-08 01:37:16 +0530

    highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import VideoCarousel from './VideoCarousel';
 
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+  }, []);
   return (
-    <div>
-      
-    </div>
+    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className='screen-max-width'>
+        <div className='mb-12 w-full md:flex items-end justify-between'>
+          <h1 id = "title" className='section-heading'>Get the highlights.</h1>
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link'>
+              Watch the film
+              <img src = "/assets/images/watch.svg" alt = "watch" className='ml-2'/>
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src = "/assets/images/right.svg"  alt = "right" className='ml-2'/>
+            </p>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
+    </section>
   )
 }
 
-export default Highlights
+export default Highlights;
