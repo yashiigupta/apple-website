db4284065712c0a4ab1ca33cd680a8f9659de55f
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-12-08 18:24:53 +0530

    how it works section and footer section completed

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
+import Lights from './Lights';
+import Model from './iPhone';
+import * as THREE from 'three';
+import Loader from './Loader';
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id = {gsapType}
+      className={`w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+      <OrbitControls 
+        makeDefault
+        ref = {controlRef}
+        enableZoom={false}
+        enablePan={true}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group ref = {groupRef} name = {`${index === 1} ? 'small' : 'large'`} position={[0, 0, 0]}>
+        <Suspense fallback={<Loader />}>
+          <Model 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size} />
+        </Suspense>
+      </group>
+    </View>
   )
 }
 
